fc84e725b646ef50ba518c5266a83ec28b457e9b
Vishal Gelani <vishalgelani99@gmail.com>
2016-07-13 08:26:24 +0530

    Initial Commit

---
+++ app/code/Namespace/Module/Model/Product/DemoType.php
@@ -0,0 +1,9 @@
+<?php
+namespace Namespace\Module\Model\Product;
+
+class DemoType extends \Magento\Catalog\Model\Product\Type\AbstractType
+{
+    public function deleteTypeSpecificData(\Magento\Catalog\Model\Product $product)
+    {
+    }
+}
